5f5f1968ba9558d5436a66cc780afbf5876f339f
shin1x1 <shin1x1@gmail.com>
2015-01-21 21:16:53 +0900

    reformat code

--- src/app/config/heroku/database.php
+++ src/app/config/heroku/database.php
@@ -4,12 +4,11 @@ $url = parse_url(getenv('DATABASE_URL'));
 
 return [
     'default' => 'pgsql',
-
     'connections' => [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => $url['host'],
-            'database' =>  substr($url['path'], 1),
+            'database' => substr($url['path'], 1),
             'username' => $url['user'],
             'password' => $url['pass'],
             'charset' => 'utf8',
--- src/app/lang/ja/validation.php
+++ src/app/lang/ja/validation.php
@@ -13,57 +13,56 @@ return [
       |
       */
 
-    "accepted"         => ":attributeが確認されていません。",
-    "active_url"       => ":attributeは無効なURLです。",
-    "after"            => ":attributeは:dateより後の日付でなければなりません。",
-    "alpha"            => ":attributeにはアルファベット以外使用できません。",
-    "alpha_dash"       => ":attributeにはアルファベット、数字、ハイフン、アンダーバー以外使用できません。",
-    "alpha_num"        => ":attributeにはアルファベット、数字以外使用できません。",
-    "before"           => ":attributeは:dateより前の日付でなければなりません。",
-    "between"          => [
+    "accepted" => ":attributeが確認されていません。",
+    "active_url" => ":attributeは無効なURLです。",
+    "after" => ":attributeは:dateより後の日付でなければなりません。",
+    "alpha" => ":attributeにはアルファベット以外使用できません。",
+    "alpha_dash" => ":attributeにはアルファベット、数字、ハイフン、アンダーバー以外使用できません。",
+    "alpha_num" => ":attributeにはアルファベット、数字以外使用できません。",
+    "before" => ":attributeは:dateより前の日付でなければなりません。",
+    "between" => [
         "numeric" => ":attributeは:min～:maxの範囲である必要があります。",
-        "file"    => ":attributeのファイルサイズは:min～:maxキロバイトの範囲である必要があります。",
-        "string"  => ":attributeの長さは:min～:max文字の範囲である必要があります。",
+        "file" => ":attributeのファイルサイズは:min～:maxキロバイトの範囲である必要があります。",
+        "string" => ":attributeの長さは:min～:max文字の範囲である必要があります。",
     ],
-    "confirmed"        => ":attributeは確認欄と一致しませんでした。",
-    "date"             => ":attributeは正しい日付ではありません。",
-    "date_format"      => ":attributeは:format形式ではありません。",
-    "different"        => ":attributeと:otherは異なる必要があります。",
-    "digits"           => ":attributeは:digits桁である必要があります。",
-    "digits_between"   => ":attributeは:min～:max桁の範囲である必要があります。",
-    "email"            => ":attributeは正しいメールアドレスではありません。",
-    "exists"           => "選択された:attributeは存在しませんでした。",
-    "image"            => ":attributeは画像ファイルである必要があります。",
-    "in"               => "選択された:attributeは正しくありません。",
-    "integer"          => ":attributeは整数である必要があります。",
-    "ip"               => ":attributeは正しいIPアドレスではありません。",
-    "max"              => [
+    "confirmed" => ":attributeは確認欄と一致しませんでした。",
+    "date" => ":attributeは正しい日付ではありません。",
+    "date_format" => ":attributeは:format形式ではありません。",
+    "different" => ":attributeと:otherは異なる必要があります。",
+    "digits" => ":attributeは:digits桁である必要があります。",
+    "digits_between" => ":attributeは:min～:max桁の範囲である必要があります。",
+    "email" => ":attributeは正しいメールアドレスではありません。",
+    "exists" => "選択された:attributeは存在しませんでした。",
+    "image" => ":attributeは画像ファイルである必要があります。",
+    "in" => "選択された:attributeは正しくありません。",
+    "integer" => ":attributeは整数である必要があります。",
+    "ip" => ":attributeは正しいIPアドレスではありません。",
+    "max" => [
         "numeric" => ":attributeは:max以下である必要があります。",
-        "file"    => ":attributeのファイルサイズは:maxキロバイト以下である必要があります。",
-        "string"  => ":attributeの長さは:max文字以下である必要があります。",
+        "file" => ":attributeのファイルサイズは:maxキロバイト以下である必要があります。",
+        "string" => ":attributeの長さは:max文字以下である必要があります。",
     ],
-    "mimes"            => ":attributeのファイル種別は:valuesである必要があります。",
-    "min"              => [
+    "mimes" => ":attributeのファイル種別は:valuesである必要があります。",
+    "min" => [
         "numeric" => ":attributeは:min以上である必要があります。",
-        "file"    => ":attributeのファイルサイズは:minキロバイト以上である必要があります。",
-        "string"  => ":attributeの長さは :min文字以上である必要があります。",
+        "file" => ":attributeのファイルサイズは:minキロバイト以上である必要があります。",
+        "string" => ":attributeの長さは :min文字以上である必要があります。",
     ],
-    "not_in"           => "選択された:attributeは正しくありません。",
-    "numeric"          => ":attributeは数値である必要があります。",
-    "regex"            => ":attributeの形式は正しくありません。",
-    "required"         => ":attributeは必須です。",
-    "required_if"      => ":otherが:valueである場合、:attributeは必須です。",
-    "required_with"    => ":valuesが指定されている場合、:attributeは必須です。",
+    "not_in" => "選択された:attributeは正しくありません。",
+    "numeric" => ":attributeは数値である必要があります。",
+    "regex" => ":attributeの形式は正しくありません。",
+    "required" => ":attributeは必須です。",
+    "required_if" => ":otherが:valueである場合、:attributeは必須です。",
+    "required_with" => ":valuesが指定されている場合、:attributeは必須です。",
     "required_without" => ":valuesが指定されていない場合、:attributeは必須です。",
-    "same"             => ":attributeと:otherが一致しません。",
-    "size"             => [
+    "same" => ":attributeと:otherが一致しません。",
+    "size" => [
         "numeric" => ":attributeは:sizeである必要があります。",
-        "file"    => ":attributeのファイルサイズは:sizeキロバイトである必要があります。",
-        "string"  => ":attributeの長さは:size文字である必要があります。",
+        "file" => ":attributeのファイルサイズは:sizeキロバイトである必要があります。",
+        "string" => ":attributeの長さは:size文字である必要があります。",
     ],
-    "unique"           => ":attributeはすでに使われています。",
-    "url"              => ":attributeは正しいURL形式ではありません。",
-
+    "unique" => ":attributeはすでに使われています。",
+    "url" => ":attributeは正しいURL形式ではありません。",
     /*
     |--------------------------------------------------------------------------
     | Custom Validation Language Lines
@@ -76,7 +75,6 @@ return [
     */
 
     'custom' => [],
-
     /*
     |--------------------------------------------------------------------------
     | Custom Validation Attributes
