cf452276f36328e02e6d415fd1d8a86a3120d2dc
shin1x1 <shin1x1@gmail.com>
2015-01-21 21:16:34 +0900

    change database engine to mysql

--- src/app/config/local/database.php
+++ src/app/config/local/database.php
@@ -1,27 +1,17 @@
 <?php
 
 return [
-    'default' => 'pgsql',
+    'default' => 'mysql',
     'connections' => [
         'mysql' => [
             'driver' => 'mysql',
             'host' => 'localhost',
             'database' => 'app',
             'username' => 'vagrant',
-            'password' => 'secret',
+            'password' => 'pass',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
-        ],
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => 'localhost',
-            'database' => 'app',
-            'username' => 'vagrant',
-            'password' => 'secret',
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
         ],
     ],
 ];
